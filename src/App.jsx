7506818102229c9b7a9b6801c3d5a15b6321bcb5
JordiPalacios <jordipalacioseso@gmail.com>
2025-01-30 20:08:08 +0100

    slider done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+      <h1 className='font-semibold text-emerald-500 underline my-3 p-1'>React Componets</h1>
       <Carousel_00 />
       <div className='h-1 w-full bg-white my-5'></div>
     </>
--- src/components/Carousel/Carousel_00.jsx
+++ src/components/Carousel/Carousel_00.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import CarouselData from "./mediaMock.json"
+import { Carousel_BlackBox } from './Carousel_BlackBox';
 
 export const Carousel_00 = () => {
+  const carouselData = CarouselData
 
-  var settings = {
+  var sliderSettings ={
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-  };
+  }
 
   return (
-    <Slider {...settings}>
-      <div>
-        <video src="src/components/Carousel/media/videoMobile.mov" controls/>
-      </div>
-      <div>
-        <video src="src/components/Carousel/media/videoMobile.mov" controls/>
-      </div>
-      <div>
-        <video src="src/components/Carousel/media/videoMobile.mov" controls/>
-      </div>
-      <div>
-        <video src="src/components/Carousel/media/videoMobile.mov" controls/>
-      </div>
-    </Slider>
+    <div>
+      <h1 className='font-semibold text-emerald-300 my-1'>Carousel</h1>
+      <Slider {...sliderSettings}>
+        {carouselData.map((data) => (
+          <Carousel_BlackBox
+          name={data.name}
+          url={data.url}
+          styleType="example3"
+          isVideo={data.isVideo}
+          />
+        ))}
+      </Slider>
+    </div>
   )
 }
 
